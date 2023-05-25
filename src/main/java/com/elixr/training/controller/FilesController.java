708923da0259564f30c4697010a02a584ec6b3e2
Arun Yesuraj <arun.yesuraj@theelixrlabs.com>
2023-05-25 15:33:58 +0530

    Exception handler enabled

--- src/main/java/com/elixr/training/controller/FilesController.java
+++ src/main/java/com/elixr/training/controller/FilesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 
 @RestController
 public class FilesController {
@@ -17,15 +18,9 @@ public class FilesController {
     FileStorageService storageService;
 
     @PostMapping("file/upload")
-    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
-        String message = "";
-        try {
-            storageService.save(file);
-            message = "Uploaded the file successfully: " + file.getOriginalFilename();
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-        } catch (Exception e) {
-            message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-        }
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        storageService.save(file);
+        String  message = "Uploaded the file successfully: " + file.getOriginalFilename();
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     }
 }
