4893043915b56c0eb7ca7755628ae95cf0327ad7
Arun Yesuraj <arun.yesuraj@theelixrlabs.com>
2023-06-13 14:25:35 +0530

    context load test case issue fix

--- src/test/java/com/elixr/training/TrainingApplicationTests.java
+++ src/test/java/com/elixr/training/TrainingApplicationTests.java
@@ -1,9 +1,12 @@
 package com.elixr.training;
 
+import io.micrometer.tracing.Tracer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = { Tracer.class})
 class TrainingApplicationTests {
 
 	@Test
